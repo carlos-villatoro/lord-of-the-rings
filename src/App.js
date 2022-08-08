c7f07b1168f7995f8d3668043ce35af00458b0f3
carlos-villatoro <futurebound.carlosv@gmail.com>
2022-08-08 13:24:29 -0500

    init commit

--- src/App.js
+++ src/App.js
@@ -1,25 +1,72 @@
-import logo from './logo.svg';
+import React,{ Component } from 'react';
 import './App.css';
+import Movie from './Movie';
+
+
+const movies = [
+  {
+    title:"The Fellowship of the Ring",
+    hours: 2,
+    minutes: 58
+  },
+  {
+    title:"The Two Towers",
+    hours: 2,
+    minutes: 59
+  },
+  {
+    title:"The Return of the King",
+    hours: 3,
+    minutes: 21
+  },
+]
+
+class App extends Component{
+  render(){
+      // map movie data into an array
+      const movieComponents= movies.map((movie, index) =>{
+        return (
+          <Movie
+          key={`movie-${index}`}
+          title={movie.title}
+          hours={movie.hours}
+          minutes={movie.minutes}
+          />
+        )
+      })
+    return (
+      <div className="App">
+        <h1>The Lord of the Rings: A Trilogy</h1>
+        <p>4h 37min</p>
+        {movieComponents}
+
+    </div>
+    )
+  }
+}
+
+/* 
+  // map movie data into an array
+const movieComponents= movies.map(movie =>{
+  return (
+    <Movie
+    title={movie.title}
+    hours={movie.hours}
+    minutes={movie.minutes}
+    />
+  )
+})
 
 function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
-  );
+return (
+  <div className="App">
+    <h1>The Lord of the Rings: A Trilogy</h1>
+    <p>4h 37min</p>
+    {movieComponents}
+
+  </div>
+);
 }
+*/
 
 export default App;
